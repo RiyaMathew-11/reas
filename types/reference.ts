7ddd6a1be11f9009f65639aea734746b510f39c3
RiyaMathew-11 <riyapmathew2000@gmail.com>
2026-02-01 16:59:31 +0000

    feat: add access-date and url to references

--- types/reference.ts
+++ types/reference.ts
@@ -10,34 +10,34 @@ export interface Author {
 export interface BaseReference {
   id: string
   type: ReferenceType
-  authors: Author[]
-  year: string
+  authors?: Author[]
+  year?: string
+  url?: string
+  accessDate?: string
 }
 
 export interface JournalReference extends BaseReference {
   type: 'journal'
-  articleTitle: string
-  journalName: string
-  volume: string
-  issue: string
-  pageRange: string
+  articleTitle?: string
+  journalName?: string
+  volume?: string
+  issue?: string
+  pageRange?: string
   doi?: string
 }
 
 export interface BookReference extends BaseReference {
   type: 'book'
-  bookTitle: string
-  publisher: string
-  placeOfPublication: string
+  bookTitle?: string
+  publisher?: string
+  placeOfPublication?: string
   edition?: string
 }
 
 export interface WebsiteReference extends BaseReference {
   type: 'website'
-  pageTitle: string
-  websiteName: string
-  url: string
-  accessDate: string
+  pageTitle?: string
+  websiteName?: string
 }
 
 export type Reference = JournalReference | BookReference | WebsiteReference
